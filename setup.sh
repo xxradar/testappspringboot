19094fb1602b76845720441ccc26249d51ffce5e
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2021-07-26 13:42:38 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y git unzip
+
+
+# echo "Installing Docker ..."
+curl -fsSL https://get.docker.com -o get-docker.sh
+chmod 770 ./get-docker.sh
+./get-docker.sh
+
+# echo "Installing JDK  ----"
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt-get update
+sudo apt install -y oracle-java16-installer
+java -version
+
+# echo "Installing Maven ----"
+wget https://downloads.apache.org/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.tar.gz
+tar xzvf apache-maven-3.8.1-bin.tar.gz
+export PATH="$HOME/apache-maven-3.8.1/bin:$PATH"
+mvn  -v
+
+
+# echo "Getting Demo app  ----"
+git clone https://github.com/xxradar/testappspringboot.git
+cd testappspringboot/
+unzip demo.zip
+
